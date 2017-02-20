071059a6eeb8063fa5876fa0d8ab8afdd0832ba2
201-12 <201-12@201-12-PC>
2017-02-20 15:16:30 +0900

    -initialized oracle connection pool & DriverManagerDataSource  (and JUnit4 for test)

---
+++ mypet/src/test/java/com/mypet/controller/DataSourceTest.java
@@ -0,0 +1,35 @@
+package com.mypet.controller;
+
+import java.sql.Connection;
+
+import javax.inject.Inject;
+import javax.sql.DataSource;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+
+/**
+ * Test code : DataSource and Connection 
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+@ContextConfiguration(locations={"file:src/main/webapp/WEB-INF/spring/**/root-context.xml"})
+public class DataSourceTest {
+	Logger logger = LoggerFactory.getLogger(DataSourceTest.class);	
+	@Inject
+	DataSource ds;	
+	@Test
+	public void dataSourceTest() {
+		try(Connection conn = ds.getConnection()) {
+			logger.info(conn.toString());
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
