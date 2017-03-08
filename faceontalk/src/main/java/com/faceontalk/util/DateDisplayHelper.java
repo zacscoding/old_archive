6a7b4871eea46e0bcabb53b357a57d01d9922a32
zaccoding <zaccoding725@gmail.com>
2017-03-08 21:49:35 +0900

    -update code

---
+++ faceontalk/src/main/java/com/faceontalk/util/DateDisplayHelper.java
@@ -0,0 +1,14 @@
+package com.faceontalk.util;
+
+import java.util.Date;
+
+public class DateDisplayHelper {
+	
+	
+	public static String getDisplayDate(Date regDate) {
+		Date date = new Date(System.currentTimeMillis());
+		
+		return null;
+	}
+	
+}
