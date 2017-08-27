4825e1303d594b0fa2174af1415bdb516fd4d07c
mskim <zaccoding725@gmail.com>
2017-08-27 23:39:16 +0900

    datasource 관련 bean 설정

---
+++ test_web_app/src/main/java/com/web/controller/BasicCrudController.java
@@ -0,0 +1,11 @@
+package com.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/crud-test/**")
+public class BasicCrudController {
+	
+
+}
---
+++ test_web_app/src/main/java/com/web/mapper/oracle/v11g/Oracle11gMapper.java
@@ -0,0 +1,6 @@
+package com.web.mapper.oracle.v11g;
+
+public interface Oracle11gMapper {
+	
+	
+}
